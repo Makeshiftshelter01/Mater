1aa03653fe4088cc416da08ba7e3d040a526d15c
ul8ksgdmy <clickmy7@gmail.com>
2018-12-22 19:14:52 +0900

    ruri_dao - insertone() modified, select() merged from

--- ruri_dao.py
+++ ruri_dao.py
@@ -3,6 +3,7 @@ from ruri_connect import ConnectTo
 from ruri_config import Config
 import json
 import os
+
 import platform
 
 class CrwalingDAO:
@@ -22,22 +23,32 @@ class CrwalingDAO:
         return cnct
 
 
-    def insertone(self, cr):
-        innerurl = os.path.join(os.path.relpath(os.path.dirname(__file__)))
+    def insertone(self, cr, startini = 0, endini = 6):
+        config = Config()
+
+        # 크롤링 CSS를 가져오려 했는데 설정하는 기능이 없어 우선 ruriweb을 넣어 임시로 만듦.
+        tmpruri = config.read_info_in_config('ruriweb')
+        keys = list(tmpruri.keys())
+        values = list(tmpruri.values())
+
         conn = self.setdbinfo() #접속값을 받아옴.
-        ### 자료를 얼마나 모았을까? ###
-        print('titles this crawler has collected till now : ', len(cr[2]))
-        for i in range(0, len(cr[2])):
-            ruri = {
-                'no' : cr[0][i],
-                'html' : cr[1][i],
-                'title' : cr[2][i],
-                'thumbup' : cr[3][i],
-                'thumbdown' : cr[4][i],
-                'date' : cr[5][i],
-                'content' : cr[6][i]
-            }
-            doc_id = conn.m_collection.insert_one(ruri).inserted_id
+        upper = cr[0]
+        lower = cr[1]
+        exup = sorted(upper[0])
+        
+        print('titles this crawler has collected till now : ', len(exup)) ### 자료를 얼마나 모았을까? ###
+
+        # 몽고DB에 입력
+        for i in range(0, len(exup)):
+            mongoDict = {}
+            for j in range(startini, endini-1):
+                # j를 이용해서 키 값을 넣을 때는 url과 head가 0, 1번을 차기하고 있기 때문에 +2를 넣어준다.
+                mongoDict[keys[j+2]] = upper[j][i]
+            
+            # 컨텐츠 추가
+            mongoDict['content'] = lower[i]
+
+            doc_id = conn.m_collection.insert_one(mongoDict).inserted_id
             print('no.', i+1, 'inserted id in mongodb : ', doc_id)
 
         conn.m_client.close()

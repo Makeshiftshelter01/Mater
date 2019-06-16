edd5d5526da2bafb9983349750c26c0abdbe9d22
ksm503 <ksm503@gmail.com>
2019-06-16 16:10:27 +0900

    모든 커뮤니티 포함

--- ruri_main.py
+++ ruri_main.py
@@ -35,45 +35,52 @@ import email_module
 # logger.critical('critical')
 
 
-
-#변수
+# 변수
 # collection = 'crawl_test'
 # target = 'theqoo' #필수
 
-collection_list = ['fm_test1', 'fm_test1', 'fm_test1']
-target_list = ['clien', 'theqoo', 'inven']
+collection_list = ['realcook', 'ilbe',
+                   'realmlbpark', 'ygosu2', 'ruri', 'realclien']
+target_list = ['cook', 'ilbe', 'MPark', 'ygosu2', 'ruriweb', 'clien']
 
-firstpage =1 #옵션
-lastpage =3 #필수
+firstpage = 1  # 옵션
+lastpage = 3000  # 필수
 # dividePages는 n개로 구간을 나눠 크롤링.
 # 만약 구간이 n개로 나눠지지 않을 경우 나머지를 처리하기 위해 1회 더 실행
-dividePages = lastpage - firstpage #옵션
+dividePages = 300  # 옵션
 
 # 크롤링
-cr = Crawling() #크롤링
+cr = Crawling()  # 크롤링
 # cd = CrwalingDAO() #현재는 mongoDB
 # cd.insert(cr.crawling('ruriweb', 10)) #사용금지
 
-body = '' # 이메일 본문
+body = ''  # 이메일 본문
 
-
+st = time.time()
 for i in range(len(collection_list)):
     try:
         # 프로그램 시작 측정
         start_time = time.time()
         target = target_list[i]
         collection = collection_list[i]
-        cr.crawling(collection, target, lastpage, dividePages, firstpage) # (콜렉션명,목표, 마지막페이지, (옵션 : 원하는 페이지 분할 개수), (옵션 : 첫 번째 페이지))
+        # (콜렉션명,목표, 마지막페이지, (옵션 : 원하는 페이지 분할 개수), (옵션 : 첫 번째 페이지))
+        cr.crawling(collection, target, lastpage, dividePages, firstpage)
 
         # 프로그램 종료 측정 및 결과 출력
-        print('It takes %s seconds completing the crawling and the uploading' % (round(time.time() - start_time,2)))
-        print('------------------------------------------------------------------------------')
+        print('It takes %s seconds completing the crawling and the uploading' %
+              (round(time.time() - start_time, 2)))
+        print(
+            '------------------------------------------------------------------------------')
 
-        body +=  target + ' / STATUS : SUCCESSFUL / ' +'Total Time Consumed: ' + str(round(time.time() - start_time,2)) + '\n' #하나의 커뮤니티가 성공하면 본문에 상태 추가
-            
+        body += target + ' / STATUS : SUCCESSFUL / ' + 'Total Time Consumed: ' + \
+            str(round(time.time() - start_time, 2)) + \
+            '\n'  # 하나의 커뮤니티가 성공하면 본문에 상태 추가
+
     except:
         print('FAILED!')
-        body +=  target + ' / STATUS : FAILED!' + '\n' #실패시에도 본문에 실패했다고 추가
+        body += target + ' / STATUS : FAILED!' + '\n'  # 실패시에도 본문에 실패했다고 추가
 
-  
-email_module.send_email('Crawling Result ', body) #크롤링 하나 끝날 때마다 메일 보내기 
+et = time.time() - st
+
+print('time consumed', et)
+email_module.send_email('Crawling Result ', body)  # 크롤링 하나 끝날 때마다 메일 보내기
